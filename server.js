1d53e35142826b48a8eec2a1f77eccacd6688e3c
bcabraham <abraham.bryan@gmail.com>
2019-06-30 17:28:18 -0600

    intial settings and basic server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const fs = require("fs");
+
+var app = express();
+const PORT = process.env.PORT || 3001;
+
+// app.use(express.static(__dirname + "/public")); // dirname always holds the absolute path to the current directory the app is in.
+
+// Serve landing page
+app.get("/", (req, res) => {
+  res.send("<h2>hello there</h2>");
+});
+
+app.listen(PORT, () => {
+  console.log(`Started server on port ${PORT}`); // eslint-disable-line no-console
+});
+
+module.exports = { app };
